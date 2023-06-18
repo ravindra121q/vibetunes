2be1d17cd35d3c566459a4e536af3fad1f3160cf
Ravindra Kumar Tilotia <ravindrakumartilotia4@gmail.com>
2023-06-18 13:01:46 +0530

    backend

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const { userRouter } = require("./routes/user");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(userRouter)
+app.use(userRouter);
 
 app.use("/users", userRouter);
 
-app.listen(8080,async()=>{
-    try {
-        await connection
-        console.log("Connected to db at port 8080");
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+app.listen(8080, async () => {
+  try {
+    await connection;
+    console.log("Connected to db at port 8080");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
